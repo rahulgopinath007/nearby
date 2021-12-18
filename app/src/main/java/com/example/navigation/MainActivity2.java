67a058902b029b24ee9d9847c84185d1fde69dd3
PC <rahulgopinath980@gmail.com>
2021-12-18 12:14:25 +0530

    1

---
+++ app/src/main/java/com/example/navigation/MainActivity2.java
@@ -0,0 +1,44 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+        //spinner
+        Spinner mySpinner =(Spinner) findViewById(R.id.spinner1);
+        ArrayAdapter<String> myadapter= new ArrayAdapter<String>(MainActivity2.this,
+                android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.Levels));
+        myadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myadapter);
+        //confirm button
+        Button button;
+        button=(Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Service Request Posted!", Toast.LENGTH_LONG).show();
+                Intent mintent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(mintent);
+            }
+        });
+
+
+
+
+
+
